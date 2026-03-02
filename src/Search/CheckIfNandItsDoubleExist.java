13a560452dadd64c9723ad2f04af4e2dbe373ce7
VekariyaDenish <vekariyadenish12@gmail.com>
2026-03-02 13:42:11 +0530

    searching questions 2/3/26

---
+++ src/Search/CheckIfNandItsDoubleExist.java
@@ -0,0 +1,21 @@
+package Search;
+
+
+//1346. Check If N and Its Double Exist
+
+public class CheckIfNandItsDoubleExist {
+    public static void main(String[] args) {
+        int [] arr = {3,1,7,11};
+        System.out.println(checkIfExist(arr));
+    }
+    static boolean checkIfExist(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if( i != j && arr[i]== 2*arr[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
