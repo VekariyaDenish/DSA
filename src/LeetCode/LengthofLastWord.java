6053f8cb0da7ced7d82b57e6fdf71f2a5292b82f
VekariyaDenish <vekariyadenish12@gmail.com>
2026-03-22 15:09:58 +0530

    questions 22/3/26

---
+++ src/LeetCode/LengthofLastWord.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+//58. Length of Last Word
+
+public class LengthofLastWord {
+    public static void main(String[] args) {
+        String s = "   fly me   to   the moon  ";
+        System.out.println(lengthOfLastWord(s));
+
+    }
+    static int lengthOfLastWord(String s) {
+        s = s.trim();
+
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            }
+            else if (length > 0) {
+                break;
+            }
+        }
+
+        return length;
+    }
+}
