82656f792705ec4d998f47816ddeab7453846f97
VekariyaDenish <vekariyadenish12@gmail.com>
2026-08-05 12:14:28 +0530

    questions 5/8/26

---
+++ src/Extra/main.java
@@ -0,0 +1,13 @@
+package Extra;
+
+public class main {
+    public static void main(String[] args) {
+        int x =5;
+        change(5);
+        System.out.println(x);
+
+    }
+    static void change(int n){
+        n= n + 10;
+    }
+}
