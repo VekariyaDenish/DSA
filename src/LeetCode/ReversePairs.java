eefc093ee029ad8cc1992a88d028e9368b8b71e2
VekariyaDenish <vekariyadenish12@gmail.com>
2026-08-05 12:14:39 +0530

    questions 5/8/26

---
+++ src/LeetCode/ReversePairs.java
@@ -0,0 +1,12 @@
+package LeetCode;
+
+//493. Reverse Pairs
+
+public class ReversePairs {
+    public static void main(String[] args) {
+
+    }
+    static int reversePairs(int[] nums) {
+        return 1;
+    }
+}
